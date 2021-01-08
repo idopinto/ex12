f60bcd755846c6c11acff4b77fc0a22c4371a1e6
eilonjacob <eilon277@gmail.com>
2021-01-08 22:19:53 +0200

    if pinto

--- ex12_utils.py
+++ ex12_utils.py
@@ -2,7 +2,8 @@ def load_words_dict(file_path):
     print("i like banana ")
 
 def is_valid_path(board, path, words):
-    pass
+    if pinto:
+        return False
 
 
 def find_length_n_words(n, board, words):
